dbe308de5d0a11a032eb3d639831a30478509913
tsolloway <tsolloway@gmail.com>
2024-09-09 12:51:49 -0700

    Update days_months.R

--- R/days_months.R
+++ R/days_months.R
@@ -1,8 +1,17 @@
+#' today
+#' @description today
+#' @export
+today <- function(){
+  lubridate::today()
+}
+
+
+
 #' this_day
 #' @description this_day
 #' @export
 this_day <- function(){
-  Sys.Date()
+  lubridate::today() %>% lubridate::day()
 }
 
 
@@ -10,14 +19,18 @@ this_day <- function(){
 #' this_month
 #' @description this_month
 #' @export
-this_month <- function(part = c("start", "end")){
+this_month <- function(
+    part = c("number", "start", "end")
+){
 
   part <- match.arg(part)
 
-  if(part == "start"){
-    this_day() %>% lubridate::floor_date("month")
+  if(part == "number"){
+    lubridate::today() %>% lubridate::month()
+  }else if(part == "start"){
+    lubridate::today() %>% lubridate::floor_date("month")
   }else if(part == "end"){
-    this_day() %>% lubridate::ceiling_date("month") - lubridate::day(1) %>% suppressWarnings()
+    lubridate::today() %>% lubridate::ceiling_date("month") - lubridate::day(1) %>% suppressWarnings()
   }
 }
 
@@ -26,13 +39,89 @@ this_month <- function(part = c("start", "end")){
 #' last_month
 #' @description last_month
 #' @export
-last_month <- function(part = c("start", "end")){
+last_month <- function(part = c("number", "start", "end")){
 
   part <- match.arg(part)
 
-  if(part == "start"){
-    (this_month() - lubridate::month(1)) %>% lubridate::floor_date("month")
+  if(part == "number"){
+    lubridate::today() %>% lubridate::month() - 1
+  }else if(part == "start"){
+    (this_month("start") - lubridate::days(1)) %>% lubridate::floor_date("month")
   }else if(part == "end"){
-    this_month() - lubridate::month(1)
+    (this_month("start") - lubridate::days(1))
   }
 }
+
+
+
+#' this_day
+#' @description this_day
+#' @export
+this_year <- function(
+    part = c("number", "start", "end")
+){
+
+  part <- match.arg(part)
+
+  if(part == "number"){
+    lubridate::today() %>% lubridate::year()
+  }else if(part == "start"){
+    lubridate::today() %>% lubridate::floor_date("year")
+  }else if(part == "end"){
+    lubridate::today() %>% lubridate::ceiling_date("year") - lubridate::day(1) %>% suppressWarnings()
+  }
+}
+
+
+
+#' date_seq
+#' @description date_seq
+#' @export
+date_seq <- function(start, stop, interval = c("day", "month", "quarter", "year"), steps = 1){
+
+  interval <- match.arg(interval)
+
+  interval <- glue("{steps} {interval}")
+
+  seq(
+    lubridate::ymd(start),
+    lubridate::ymd(stop),
+    by = interval
+  )
+}
+
+
+
+#' calendar
+#' @description calendar
+#' @export
+calendar <- function(
+    interval = c("day", "month", "quarter", "year"),
+    first_year = this_year(),
+    last_year = this_year(),
+    steps = 1,
+    to_now = FALSE
+){
+
+  interval <- match.arg(interval)
+
+  date_start <- (glue("{first_year}-01-01"))
+  date_end <- (glue("{last_year}-12-31"))
+
+
+  result <- date_seq(date_start, date_end, interval, steps)
+
+
+  if(to_now){
+    result <- result[result <= today()]
+  }
+
+
+  return(result)
+}
+
+
+
+
+
+
